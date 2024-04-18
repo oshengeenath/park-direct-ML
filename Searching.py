c9f538cb284cce329e54bd8a821149a6db302c45
Thathsarani-Sandarekha <thathsaranisandarekha@gmail.com>
2024-04-19 01:29:38 +0530

    Updated the return data

--- Searching.py
+++ Searching.py
@@ -20,23 +20,26 @@ def check_number_plate(collection):
         today_date = datetime.now().date()
         
         # Extract data from the result
-        plate = result["Number Plate"]
-        slot = result["Parking slot"]
+        plate = result["vehicleNumber"]
+        slot = result["parkingSlotId"]
         date = result["date"]
+        arrival_time = result['arrivalTime']
+        departure_time = result['leaveTime']
+        booking_status = result['status']
         
         if date.date() == today_date:
-            print(f"Number Plate: {plate}, Parking Slot: {slot}")
+            print(f"Vehicle Number: {plate}, Parking Slot: {slot}, Arrival Time: {arrival_time}, Departure Time: {departure_time}, Booking Status: {booking_status}")
         elif date.date() > today_date:
             print("The date is in the future.")
         else:
             print("The date has expired.")
     else:
-        print("Number plate does not exist.")
+        print("Vehicle Number does not exist.")
 
 # Connect to MongoDB
-client = pymongo.MongoClient("mongodb+srv://thathsarani20211422:Bx2NtjbW3g3XYDKs@edgeai.kgqujac.mongodb.net/?retryWrites=true&w=majority&appName=edgeai")
-db = client["edgeai_test1"]
-collection = db["test_data2"]
+client = pymongo.MongoClient("mongodb+srv://arul20210434:Ze3zWYFSym1wEuuE@edgeai.2feeugm.mongodb.net/?retryWrites=true&w=majority&appName=edgeAI")
+db = client["test"]
+collection = db["bookings"]
 
 # Call the function
 check_number_plate(collection)
